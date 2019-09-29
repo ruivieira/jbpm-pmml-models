99c828648535057f846ffa551a94a09c4fa6b361
Rui Vieira <ruidevieira@googlemail.com>
2019-09-29 18:51:15 +0100

    Change model to include `price` and `item`

--- builder.py
+++ builder.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
@@ -8,22 +9,43 @@ import random
 
 data = []
 
-for i in range(10000):
-    r = random.random()
-    data.append({'ActorId': 'john', 'level': 5, 'approved': 0 if r < 0.9 else 1})
+ITERATIONS = 200
 
-    v = random.random()
-    data.append({'ActorId': 'mary', 'level': 5, 'approved': 1 if v < 0.9 else 0})
+# Approved
+lenovoPrices = np.random.normal(loc=1500, scale=40, size=ITERATIONS)
+for price in lenovoPrices:
+    data.append({'ActorId': 'John', 'item': 'Lenovo', 'price': price, 'approved': 1})
+
+lenovoPrices = np.random.normal(loc=1500, scale=40, size=ITERATIONS)
+for price in lenovoPrices:
+    data.append({'ActorId': 'Mary', 'item': 'Lenovo', 'price': price, 'approved': 1})
+
+applePrices = np.random.normal(loc=2500, scale=40, size=ITERATIONS)
+for price in applePrices:
+    data.append({'ActorId': 'John', 'item': 'Apple', 'price': price, 'approved': 1})
+
+applePrices = np.random.normal(loc=2500, scale=40, size=ITERATIONS)
+for price in applePrices:
+    data.append({'ActorId': 'Mary', 'item': 'Apple', 'price': price, 'approved': 1})
+
+# Rejected
+applePrices = np.random.normal(loc=2500, scale=40, size=ITERATIONS)
+for price in applePrices:
+    data.append({'ActorId': 'John', 'item': 'Lenovo', 'price': price, 'approved': 0})
+
+applePrices = np.random.normal(loc=2500, scale=40, size=ITERATIONS)
+for price in applePrices:
+    data.append({'ActorId': 'Mary', 'item': 'Lenovo', 'price': price, 'approved': 0})
 
 df = pd.DataFrame(data)
 df.ActorId = pd.Categorical(pd.factorize(df.ActorId)[0])
-df.level = pd.Categorical(pd.factorize(df.level)[0])
 df.approved = pd.Categorical(pd.factorize(df.approved)[0])
+df.item = pd.Categorical(pd.factorize(df.item)[0])
 
 
 # Split data
 outputs = df['approved']
-inputs = df[['ActorId', 'level']]
+inputs = df[['ActorId', 'price', 'item']]
 
 # split dataset
 X_train, X_test, y_train, y_test = train_test_split(inputs, outputs, test_size=0.4,
@@ -35,7 +57,7 @@ pipeline = Pipeline([("regressor", rf)])
 pipeline.fit(X_train, y_train)
 
 # save PMML model
-skl_to_pmml(pipeline, ['ActorId', 'level'], 'approved', "models/random_forest.pmml")
+skl_to_pmml(pipeline, ['ActorId', 'price', 'item'], 'approved', "models/random_forest.pmml")
 
 # Logistic regression model
 lr = LogisticRegression()
@@ -43,4 +65,4 @@ pipeline = Pipeline([("regressor", lr)])
 pipeline.fit(X_train, y_train)
 
 # save PMML model
-skl_to_pmml(pipeline, ['ActorId', 'level'], 'approved', "models/logistic_regression.pmml")
+skl_to_pmml(pipeline, ['ActorId', 'price', 'item'], 'approved', "models/logistic_regression.pmml")
